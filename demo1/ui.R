4c95cd4d2073a0613818311cc4f775dc48c8ecd1
mazhenkai <523395363@qq.com>
2017-08-06 21:41:24 +0800

    Update ui.R

--- demo1/ui.R
+++ demo1/ui.R
@@ -1,33 +1,23 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
 library(shiny)
-shinyUI(bootstrapPage(
-  
-  headerPanel("第一个Shiny应用"),
-  
-  # 左侧布局
-  sidebarPanel(
-    
-    # 下拉框
-    selectInput(inputId = "n_breaks",label = "直方图中的分隔数",choices = c(10, 20, 35, 50),selected = 20),
-    
-    # 单选框
-    checkboxInput(inputId = "individual_obs",label = strong("实际观察点"),value = FALSE),
-    
-    # 单选框
-    checkboxInput(inputId = "density",label = strong("密度估计曲线"),value = FALSE)
-  ),
-  
-  # 主布局
-  mainPanel(
-    plotOutput(outputId = "main_plot", height = "300px"),
-    
-    conditionalPanel(condition = "input.density == true",
-                     sliderInput(inputId = "bw_adjust",label = "带宽调整", min = 0.2, max = 2, value = 1, step = 0.2)
-    )
-  )
-))
 
+shinyServer(function(input, output) {
 
+  output$distPlot <- renderPlot({
 
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
+  })
 
+})
