55964918d822c83b5e4e8bf5ce1273ff814a3822
mazhenkai <523395363@qq.com>
2017-08-06 21:40:53 +0800

    Update server.R

--- demo1/server.R
+++ demo1/server.R
@@ -1,27 +1,25 @@
+
+
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyUI(fluidPage(
 
-  # 输出到UI的main_plot
-  output$main_plot <- renderPlot({
-    
-    # 直方图
-    hist(faithful$eruptions,
-         probability = TRUE,
-         breaks = as.numeric(input$n_breaks),
-         xlab = "持续时间",
-         main = "喷发持续时间")
-    
-    # 是否显示individual_obs
-    if (input$individual_obs) {
-      rug(faithful$eruptions)
-    }
-    
-    # 是否显示conditionalPanel
-    if (input$density) {
-      dens <- density(faithful$eruptions, adjust = input$bw_adjust)
-      lines(dens, col = "blue")
-    }
-    
-  })
-})
+  # Application title
+  titlePanel("测试一下shiny"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
