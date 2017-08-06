29cd7f0c9c4cb25b94f6e5298a5551501625472e
mazhenkai <523395363@qq.com>
2017-08-06 21:47:24 +0800

    Update ui.R

--- demo1/ui.R
+++ demo1/ui.R
@@ -1,5 +1,5 @@
 
-# This is the server logic for a Shiny web application.
+# This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
 # http://shiny.rstudio.com
@@ -7,17 +7,24 @@
 
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyUI(fluidPage(
 
-  output$distPlot <- renderPlot({
+  # Application title
+  titlePanel("测试一下shiny"),
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
 
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-  })
-
-})
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
